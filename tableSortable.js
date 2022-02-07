d174753fd29d0d5dc2366d313ec79c150d6aa92d
rpserjo <rpserjo@gmail.com>
2022-02-07 16:23:48 +0300

    Add files via upload

--- tableSortable.js
+++ tableSortable.js
@@ -1,37 +1,36 @@
-var sortableTables = document.querySelectorAll('table.sortable');
-[].forEach.call(sortableTables, (table, tableKey) => {
-	[].forEach.call(table.tHead.rows[0].cells, (cell, cellKey) => {
-		if(cell.dataset.nonsortable != 'true'){
-			cell.setAttribute('onclick', 'sortTable('+ tableKey + ',' + cellKey + ')');
+document.addEventListener('click', function(e){
+	var element = e.target;
+	var cellIndex = 0;
+	if(element.dataset.nonsortable !== 'true'){
+		if(element.parentNode.parentNode.nodeName === "THEAD"){
+			if(element.parentNode.parentNode.parentNode.classList.contains("sortable")){
+				[].forEach.call(element.parentNode.cells, function(cell, index){
+					if(cell !== element){
+						cell.classList.remove('order-field', 'order-asc', 'order-desc');
+						delete cell.dataset.order;
+					}else{
+						cellIndex = index;
+					}
+				});
+				element.classList.add('order-field');
+				var order = (element.dataset.order === undefined) ? 'desc' : element.dataset.order;
+				element.classList.remove('order-' + order);
+				order = (order === 'asc') ? 'desc' : 'asc';
+				element.classList.add('order-' + order);
+				[].slice.call(element.parentNode.parentNode.parentNode.tBodies[0].rows).sort(function(a, b){
+					var isNum = element.dataset.isnum;
+					var aVal = (order === 'asc') ? getValue(a.cells[cellIndex], isNum) : getValue(b.cells[cellIndex], isNum);
+					var bVal = (order === 'asc') ? getValue(b.cells[cellIndex], isNum) : getValue(a.cells[cellIndex], isNum);
+					return aVal < bVal ? -1 : aVal > bVal ? 1 : 0;
+				}).forEach(function(row, index){
+					element.parentNode.parentNode.parentNode.tBodies[0].appendChild(row);
+				});
+				element.dataset.order = order;
+			}
 		}
-	});
+	}
+
+	function getValue(cell, isNum){
+		return (cell.dataset.value !== undefined) ? cell.dataset.value : (isNum === 'true') ? parseInt(cell.textContent) : cell.textContent;
+	}
 });
-
-function sortTable(table, column){
-	[].forEach.call(sortableTables[table].tHead.rows[0].cells, (cell, cellKey) => {
-		if(cellKey != column){
-			cell.classList.remove('order-field', 'order-asc', 'order-desc');
-			delete cell.dataset.order;
-		}
-	});
-	var cell = sortableTables[table].tHead.rows[0].cells[column];
-	cell.classList.add('order-field');
-	var order = (cell.dataset.order == undefined) ? 'desc' : cell.dataset.order;
-	cell.classList.remove('order-' + order);
-	order = (order == 'asc') ? 'desc' : 'asc';
-	cell.classList.add('order-' + order);
-	
-	[].slice.call(sortableTables[table].tBodies[0].rows).sort((a, b) => {
-		var isNum = sortableTables[table].tHead.rows[0].cells[column].dataset.isnum;
-		var aVal = (order == 'asc') ? getValue(a.cells[column], isNum) : getValue(b.cells[column], isNum);
-		var bVal = (order == 'asc') ? getValue(b.cells[column], isNum) : getValue(a.cells[column], isNum);
-		return aVal < bVal ? -1 : aVal > bVal ? 1 : 0;
-	}).forEach((row, index) => {
-		sortableTables[table].tBodies[0].appendChild(row);
-	});
-	cell.dataset.order = order;
-}
-
-function getValue(cell, isNum){
-	return (cell.dataset.value != undefined) ? cell.dataset.value : (isNum == 'true') ? parseInt(cell.textContent) : cell.textContent;
-}
